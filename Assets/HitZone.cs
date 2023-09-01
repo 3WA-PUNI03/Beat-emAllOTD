312cefcc923c4c73169b7a385f465f7a83fb5a79
Kévin Roussel <9155865+KevinRoussel@users.noreply.github.com>
2023-09-01 10:56:35 +0200

    AI Implementation

--- Assets/HitZone.cs
+++ Assets/HitZone.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class HitZone : MonoBehaviour
 {
     [SerializeField] List<Collider2D> _colliders;
+    [SerializeField] string _tagTotrack;
+
 
     public List<Collider2D> Colliders { get => _colliders; }
 
@@ -12,18 +14,18 @@ public class HitZone : MonoBehaviour
     {
         // Si l'objet detecté n'a pas de rigidbody => on ignore il nous intéresse pas
         if (collision.attachedRigidbody == null) return;
-        // En revanche s'il a un rigidbody, donc le tag est "Enemy", là on va l'ajouter à notre liste
-        if (collision.attachedRigidbody.gameObject.CompareTag("Enemy"))
+
+        // En revanche s'il a un rigidbody, dont le tag est _tagTotrack, là on va l'ajouter à notre liste
+        if (collision.attachedRigidbody.gameObject.CompareTag(_tagTotrack))
         {
             _colliders.Add(collision);
         }
-
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.attachedRigidbody == null) return;
-        if (collision.attachedRigidbody.gameObject.CompareTag("Enemy"))
+        if (collision.attachedRigidbody.gameObject.CompareTag(_tagTotrack))
         {
             _colliders.Remove(collision);
         }
